0c7158765f506d7d19ea9da6c8278ea6d4f4c829
BJM19 <57643084+BJM19@users.noreply.github.com>
2022-02-23 14:48:05 -0500

    Código Base

---
+++ src/main/java/edu/eci/AREP/HttpServer.java
@@ -0,0 +1,64 @@
+package edu.eci.AREP;
+import java.net.*;
+import java.io.*;
+
+public class HttpServer {
+
+
+    /**
+     * Clase Servidor que conecta al cliente
+     *
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(35000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+        Socket clientSocket = null;
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String linea;
+        while ((linea = in.readLine()) != null) {
+            System.out.println(clima(linea));
+            if (linea.equals("")) {
+                System.out.println("Terminado");
+                break;
+            } else {
+                System.out.println("Entrada:" + linea);
+            }
+        }
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+
+    /**
+     * Metodo que recibe la ciudad y obtinene el JSON con las caracterisiticas
+     *
+     * @param linea ingreso del cliente
+     * @return JSON de la ciudad
+     */
+    private static String clima(String linea) throws Exception {
+        String api_key = "9aef024db157bbe617995ff3ce911c8c";
+
+        String base_url = "http://api.openweathermap.org/data/2.5/weather?";
+
+        String city_name = linea;
+
+        String complete_url = base_url + "appid=" + api_key + "&q=" + city_name;
+        String pagina = lerrinformacionurl.consultar(complete_url);
+        return pagina;
+    }
+}
---
+++ src/main/java/edu/eci/AREP/lerrinformacionurl.java
@@ -0,0 +1,27 @@
+package edu.eci.AREP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class lerrinformacionurl {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public static String consultar(String pagina) throws MalformedURLException {
+        URL site = new URL(pagina);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(site.openStream()))) {
+            String inputLine = "";
+            while ((inputLine = reader.readLine()) != null) {
+                pagina = pagina + inputLine;
+                System.out.println(inputLine);
+            }
+        } catch (IOException x) {
+            System.err.println(x);
+        }
+        return pagina;
+    }
+}
+
